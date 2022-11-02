90a74b921b906b6d9bfcc2fbef652db1c808982f
Diego Benites Lopes <dbeniteslopes@gmail.com>
2022-11-02 16:10:10 -0300

    Name refactor

--- CSTVApp/CSTVApp.swift
+++ CSTVApp/CSTVApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct CSTVAppApp: App {
+struct CSTVApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MatchesView()
         }
     }
 }
