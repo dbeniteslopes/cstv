8b93f50b34029d13235af78090dc2308f91f51a4
Diego Benites Lopes <dbeniteslopes@gmail.com>
2022-11-02 15:22:33 -0300

    Initial Commit

---
+++ CSTVApp/CSTVAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVAppApp.swift
+//  CSTVApp
+//
+//  Created by Diego Benites Lopes on 02/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CSTVAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
