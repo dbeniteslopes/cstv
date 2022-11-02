a66fd7be746589057dbf69ffd912ab662b979b98
Diego Benites Lopes <dbeniteslopes@gmail.com>
2022-11-02 16:11:32 -0300

    Added match card

--- CSTVApp/MatchesView.swift
+++ CSTVApp/MatchesView.swift
@@ -24,12 +24,17 @@ struct MatchesView: View {
             ZStack {
                 bgColor.ignoresSafeArea()
                 
-                VStack {
-                    ForEach(viewModel.matches, id: \.id) { m in
-                        Text(m.name).foregroundColor(.white)
+                ScrollView {
+                    LazyVStack(spacing: 0) {
+                        ForEach(viewModel.matches, id: \.id) { m in
+                            MatchCard(match: m)
+                                .padding([.horizontal, .top], 24)
+                        }
+                        
+                        ProgressView()
+                            .onAppear(perform: viewModel.loadMatches)
                     }
                 }
-                .onAppear(perform: viewModel.loadMatches)
             }
             .navigationTitle("Partidas")
             .navigationBarTitleDisplayMode(.large)
@@ -40,5 +45,98 @@ struct MatchesView: View {
 struct MatchesView_Previews: PreviewProvider {
     static var previews: some View {
         MatchesView()
+    }
+}
+
+struct MatchCard: View {
+    let match: Match
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Spacer()
+                
+                Text("AGORA")
+                    .foregroundColor(.white)
+                    .font(.caption)
+                    .frame(height: 25)
+                    .padding(.horizontal, 8)
+                    .background(
+                        CustomRoundedRectangle(corners: [.topRight, .bottomLeft], cornerRadius: 16)
+                            .fill(.red)
+                    )
+            }
+            
+            HStack(spacing: 12) {
+                VStack(spacing: 10) {
+                    AsyncImage(url: URL(string: match.opponents[0].imageUrl.orEmpty)) { image in
+                        image.resizable()
+                    } placeholder: {
+                        Circle()
+                            .fill(Color(red: 0.769, green: 0.769, blue: 0.769, opacity: 1))
+                            .frame(width: 60, height: 60)
+                    }
+                    .frame(maxWidth: 60, maxHeight: 60)
+                    
+                    Text(match.opponents[0].name)
+                        .foregroundColor(.white)
+                }
+                
+                Text("vs")
+                    .foregroundColor(.white)
+                
+                VStack {
+                    AsyncImage(url: URL(string: match.opponents[1].imageUrl.orEmpty)) { image in
+                        image.resizable()
+                    } placeholder: {
+                        Circle()
+                            .fill(Color(red: 0.769, green: 0.769, blue: 0.769, opacity: 1))
+                            .frame(width: 60, height: 60)
+                    }
+                    .frame(maxWidth: 60, maxHeight: 60)
+                    
+                    Text(match.opponents[1].name)
+                        .foregroundColor(.white)
+                }
+            }
+            .padding(.vertical, 18.5)
+            
+            Rectangle()
+                .fill(.white.opacity(0.2))
+                .frame(height: 1)
+            
+            HStack {
+                Circle()
+                    .fill(Color(red: 0.769, green: 0.769, blue: 0.769, opacity: 1))
+                    .frame(width: 16, height: 16)
+                Text("\(match.league.name.orEmpty) \(match.serie.name.orEmpty)")
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal, 16)
+        }
+        .frame(maxWidth: .infinity)
+        .background(
+            RoundedRectangle(cornerRadius: 16, style: .continuous)
+                .fill(Color(red: 0.153, green: 0.149, blue: 0.224, opacity: 1))
+        )
+    }
+}
+
+struct CustomRoundedRectangle: Shape {
+    
+    var corners: UIRectCorner = .allCorners
+    var cornerRadius: CGFloat = .infinity
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+        
+        return Path(path.cgPath)
+    }
+}
+
+extension Optional where Wrapped == String {
+    var orEmpty: String {
+        return self ?? ""
     }
 }
