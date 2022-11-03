5a79a4890fbf4e480f885fe3bde614764d5184f1
Diego Benites Lopes <dbeniteslopes@gmail.com>
2022-11-03 00:08:01 -0300

    Move models to separate files

---
+++ CSTVApp/Services/Models/MatchOpponents.swift
@@ -0,0 +1,12 @@
+//
+//  MatchOpponents.swift
+//  CSTVApp
+//
+//  Created by Diego Benites Lopes on 03/11/22.
+//
+
+import Foundation
+
+struct MatchOpponents: Decodable {
+    let opponents: [Team]
+}
